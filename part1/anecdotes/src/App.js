cfcdab8078858acd033f20a1a07a9d3af521cd5a
hexudong8670 <1514133606@qq.com>
2023-01-09 17:15:20 +0800

    feat: add anecdotes

---
+++ part1/anecdotes/src/App.js
@@ -0,0 +1,70 @@
+import { useState } from "react";
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+);
+
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients",
+  ];
+
+  const getInitArr = (len) => {
+    const pointsArr = new Array(len);
+    pointsArr.fill(0);
+    return pointsArr;
+  };
+
+  const [selected, setSelected] = useState(0);
+  const [points, setPoints] = useState(getInitArr(anecdotes.length));
+
+  const getMostPopular = () => {
+    let maxIdx = 0;
+    let maxValue = points[0];
+    points.forEach((point, idx) => {
+      if (point > maxValue) {
+        maxIdx = idx;
+        maxValue = point;
+      }
+    });
+    return maxIdx;
+  };
+
+  return (
+    <>
+      <div>
+        <h1>Anecdote of the day</h1>
+        <div>{anecdotes[selected]}</div>
+        <p>has {points[selected]} votes</p>
+        <p>
+          <Button
+            text="vote"
+            handleClick={() => {
+              const copy = [...points];
+              copy[selected] += 1;
+              setPoints(copy);
+            }}
+          />
+          <Button
+            text="next anecdote"
+            handleClick={() => {
+              setSelected(Math.floor(Math.random() * 7));
+            }}
+          />
+        </p>
+      </div>
+      <div>
+        <h1>Anecdote with most votes</h1>
+        <div>{anecdotes[getMostPopular()]}</div>
+      </div>
+    </>
+  );
+};
+
+export default App;
