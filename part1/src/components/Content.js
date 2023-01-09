af5539ccbda6897e3f9f921495425623839de6ce
hexudong8670 <1514133606@qq.com>
2023-01-09 14:01:44 +0800

    feat: exe1.3~1.5

--- part1/src/components/Content.js
+++ part1/src/components/Content.js
@@ -1,13 +1,10 @@
 import Part from "./Part";
 
 const Content = (props) => {
-  return (
-    <>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
-    </>
-  );
+  const parts = props.parts.map((part) => {
+    return <Part part={part} key={part.name} />;
+  });
+  return <>{parts}</>;
 };
 
 export default Content;
