20ef3bee924f437d60f9a18628c5ccb3323da2c9
hexudong8670 <1514133606@qq.com>
2023-01-10 17:30:16 +0800

    feat: use openweathermap

--- part2/dataforcountries/src/components/Country.js
+++ part2/dataforcountries/src/components/Country.js
@@ -34,36 +34,10 @@ const Weather = ({ capital, capitalInfo }) => {
   const [weatherInfo, setWeatherInfo] = useState(null);
 
   useEffect(() => {
-    // const api_key = process.env.REACT_APP_API_KEY;
-    // const weatherUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${capitalInfo.latlng[0]}&lon=${capitalInfo.latlng[1]}&appid=${api_key}&units=metric`;
-    // axios.get(weatherUrl).then((response) => {
-    //   console.log(response.data);
-    // });
-    setWeatherInfo({
-      current: {
-        dt: 1646318698,
-        sunrise: 1646306882,
-        sunset: 1646347929,
-        temp: 282.21,
-        feels_like: 278.41,
-        pressure: 1014,
-        humidity: 65,
-        dew_point: 275.99,
-        uvi: 2.55,
-        clouds: 40,
-        visibility: 10000,
-        wind_speed: 8.75,
-        wind_deg: 360,
-        wind_gust: 13.89,
-        weather: [
-          {
-            id: 802,
-            main: "Clouds",
-            description: "scattered clouds",
-            icon: "03d",
-          },
-        ],
-      },
+    const api_key = process.env.REACT_APP_API_KEY;
+    const weatherUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${capitalInfo.latlng[0]}&lon=${capitalInfo.latlng[1]}&appid=${api_key}&units=metric`;
+    axios.get(weatherUrl).then((response) => {
+      setWeatherInfo(response.data);
     });
   }, [capitalInfo]);
   if (weatherInfo) {
